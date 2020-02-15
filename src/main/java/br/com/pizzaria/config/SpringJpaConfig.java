071655c765ccefab375513c94bdcdd701d4f8c3c
erivelto21 <eri_ximenes@hotmail.com>
2020-02-15 15:17:02 -0300

    setting useSSL false at jpa config

--- src/main/java/br/com/pizzaria/config/SpringJpaConfig.java
+++ src/main/java/br/com/pizzaria/config/SpringJpaConfig.java
@@ -19,7 +19,7 @@ public class SpringJpaConfig {
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/pizzariadb?createDatabaseIfNotExist=true");
+        ds.setUrl("jdbc:mysql://localhost:3306/pizzariadb?createDatabaseIfNotExist=true&useSSL=false");
         ds.setUsername("root");
         ds.setPassword("root123");
         return ds;
